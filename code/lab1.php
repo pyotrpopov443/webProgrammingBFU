04dd50e3c7e05f9e4163e0d2fbf449fa169445c2
petr <pyotr.popov443@mail.ru>
2022-02-13 23:01:36 +0200

    link access to variable

---
+++ code/lab1.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Imagine a lot of code here */
+
+$very_bad_unclear_name = "15 chicken wings";
+
+//Write your code here:
+
+$order = &$very_bad_unclear_name;
+$order .= " and coca-cola";
+
+//Don't change the line below
+
+echo "<br>Your order is: {$very_bad_unclear_name}.";
