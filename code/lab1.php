48b7e971fa54e617ec550cbd784354d63b637a28
petr <pyotr.popov443@mail.ru>
2022-02-19 21:13:30 +0200

    numbers

--- code/lab1.php
+++ code/lab1.php
@@ -1,5 +1,14 @@
 <?php
 
+function println($out = '')
+{
+	echo $out . '<br>';
+}
+
+/*=======================
+1) Доступ по ссылке
+=======================*/
+
 /* Imagine a lot of code here */
 
 $very_bad_unclear_name = "15 chicken wings";
@@ -12,3 +21,21 @@ $order .= " and coca-cola";
 //Don't change the line below
 
 echo "<br>Your order is: {$very_bad_unclear_name}.";
+
+/*=======================
+2) Числа
+=======================*/
+
+println();
+
+$int = 3;
+println($int);
+
+$float = 1.23;
+println($float);
+
+println(2+10);
+
+$lastMonth = 1187.23;
+$thisMonth = 1089.98;
+println($lastMonth - $thisMonth);
