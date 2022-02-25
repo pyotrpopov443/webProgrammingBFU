31f52536d1426e310d82174d62b9363c9341539a
petr <pyotr.popov443@mail.ru>
2022-02-25 22:24:50 +0200

    regex b.

--- code/lab2/regex.php
+++ code/lab2/regex.php
@@ -7,3 +7,18 @@
 $str = 'ahb acb aeb aeeb adcb axeb';
 
 preg_match_all('/a\w{2}b/', $str, $matches);
+
+/*=======================
+Задание b.
+=======================*/
+
+$str = 'a1b2c3';
+
+$str = preg_replace_callback(
+	'/\d+/',
+	static function($matches)
+	{
+		return $matches[0] ** 3;
+	},
+	$str
+);
